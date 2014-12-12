4d29a8c67a9be30c9e4a1348b598220aba9dde9b
Tim (work) <timw@calrec-2349.calrecaudio.com>
2014-12-12 08:45:27 +0000

    Initial commit

---
+++ Source1/GitFlow.cpp
@@ -0,0 +1,21 @@
+
+void DoSomethingReallyExciting()
+{
+	FlowYourBoat();
+	BreakWind();
+}
+
+void main()
+{
+	// This is a.cpp file
+	DoSomethingReallyExciting();	// typo fixed
+	PreBlow();
+	NewCode();
+	EekGonnaBlow();
+	Feature11PostMove();
+}
+
+void Progression()
+{
+	// KeepMoving on forward
+}
