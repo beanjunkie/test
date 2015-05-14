5bb1ea767869d2caed1e06bdcfa16d8010ee3f31
Tim (work) <timw@calrec-2349.calrecaudio.com>
2015-05-14 10:45:16 +0100

    Changed APFeature1

--- Source1/APFeature1.cpp
+++ Source1/APFeature1.cpp
@@ -1,26 +1,4 @@
 
-void DoSomethingReallyExciting()
+DoSomethingDifferent()
 {
-	BreakWind();
-	NewCode2();
-}
-
-void main()
-{
-	// This is a.cpp file
-	DoSomethingReallyExciting();	// typo fixed
-	EekGonnaBlow();
-	Feature11PostMove();
-	AndysFix();
-	TimsFunction();
-}
-
-void TheWayToAmarillo()
-{
-	if( ShowMe() )
-	{
-		GoThere();
-		GoHere();
-	}
-	Feature25();
 }
